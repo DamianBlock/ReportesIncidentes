0c7824ba1dcea1720b20f3f13b79d77bcba88c5e
DamianBlock <damianfrt25@gmail.com>
2023-11-20 05:06:32 -0300

    Entity manager

--- src/main/java/org/example/Operador.java
+++ src/main/java/org/example/Operador.java
@@ -1,8 +1,17 @@
 package org.example;
 import javax.persistence.Entity;
+import javax.persistence.*;
+
 
 @Entity
+@Table(name="operador")
 public class Operador {
+
+    @Id
+    @Column(name = "idOPERADOR")
+    private int id;
+    @Column(name = "nombre")
+    private String nombre;
 
 
 }
