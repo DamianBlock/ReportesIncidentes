4ec72df5a2d3014da939db9dc97f54ce23bb86a2
DamianBlock <damianfrt25@gmail.com>
2023-11-20 04:52:33 -0300

    Creando clases

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.*;
+
+public class Main {
+
+        public static EntityManager getEntityManager() {
+            EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA_PU");
+            EntityManager manager = factory.createEntityManager();
+            return manager;
+        }
+
+        public static void main(String[] args) {
+            EntityManager em = getEntityManager();
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+            Operador empleado = new Operador(); //← Seteamos todos los atributos
+            em.persist(empleado);
+            tx.commit();
+
+        }
+
+}
